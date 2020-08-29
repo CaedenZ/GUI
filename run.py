a8bc498c2ae6100a94cff6c54fb5be9a8f15ea2f
CaedenZ <fengye16@ichat.sp.edu.sg>
2020-08-29 22:24:11 +0800

    source

--- run.py
+++ run.py
@@ -1,11 +1,33 @@
-import tkinter
+import tornado.web
+import tornado.ioloop
 
-root = tkinter.Tk()
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world!!!!!!")
 
+class resourceRequestHandler(tornado.web.RequestHandler):
+    def get(self, id):
+        self.write("Querying tweet with id " + id)
 
-root.title("demo_Tkinter")
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        n = int(self.get_argument("n"))
+        r = "odd" if n % 2 else "even"
+        
+        self.write("the number " + str(n) + " is " + r)
 
-root.geometry("400x400")
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", basicRequestHandler),
+        (r"/blog", staticRequestHandler),
+        (r"/isEven", queryStringRequestHandler),
+        (r"/tweet/([0-9]+)", resourceRequestHandler)
+    ])
 
-root.mainloop()
+    app.listen(8881)
+    print("I'm listening on port 8881")
+    tornado.ioloop.IOLoop.current().start()
